bf5a841eed695b3a816a35a852500e795181d360
cleland <clelandgt@163.com>
2019-03-25 00:10:36 +0800

    初次使用maven

---
+++ mvn_sample/src/main/java/club/cleland/mvn_sample/HelloWorld.java
@@ -0,0 +1,10 @@
+package club.cleland.mvn_sample;
+
+public class HelloWorld {
+    public String sayHello(){
+        return "Hello Maven";
+    }
+    public static void main(String[] args){
+        System.out.print(new HelloWorld().sayHello());
+    }
+}
